b44d59ca1611d19412263a125b2d915729199c84
igaimerca <igaimerca@gmail.com>
2022-08-17 11:19:24 +0200

    loopstudios

---
+++ website-projects/loopstudios/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Josefin Sans', 'sans-serif'],
+                alata: ['Alata'],
+                
+            } 
+        },
+    },
+    plugins: [],
+}
